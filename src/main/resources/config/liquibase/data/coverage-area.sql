ebeb837059b3c1916cb3667010383ff269b03b37
Wallace <dev_kollen@outlook.com>
2024-05-20 16:05:46 -0300

    chore: Improving liquibase schema

---
+++ src/main/resources/config/liquibase/data/coverage-area.sql
@@ -0,0 +1,7 @@
+INSERT INTO coverage_area (id, partner_id, type, coordinates)
+VALUES (
+    '5d31bcc1-cfe7-45af-b4a5-f7d827b8f63f',
+    '9be9c025-270a-4f50-8381-807b2eaaf00c',
+    'MultiPolygon',
+    '{"type": "MultiPolygon", "coordinates": [[[[-46.57421, -21.785741], [-46.57321, -21.785441], [-46.57221, -21.784141], [-46.57421, -21.785741]]]]}'
+);
---
+++ src/main/resources/config/liquibase/data/partner.sql
@@ -0,0 +1,7 @@
+INSERT INTO partner (id, trading_name, owner_name, document)
+VALUES (
+    '9be9c025-270a-4f50-8381-807b2eaaf00c',
+    'Adega da Cerveja - Pinheiros',
+    'Zé da Silva',
+    '1432132123891/0001'
+   );
