c4f5f064a20ef640e63a7a1b1e214da8442e6dc6
Wallace <dev_kollen@outlook.com>
2024-05-20 13:52:01 -0300

    First commit: Project Config

---
+++ src/main/java/com/wallace/deliverychallenge/DeliveryChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.wallace.deliverychallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryChallengeApplication.class, args);
+	}
+
+}
